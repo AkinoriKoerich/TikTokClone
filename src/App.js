bfff4179999a04e434919c4c8f85df1a408572ff
AkinoriKoerich <yuukko404@gmail.com>
2023-04-01 05:45:06 -0300

    Atualização com banco de dados

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
           messages={222}
           shares={333}
           name="AkinoriKoerich"
-          description="Gato sendo filho da puta"
-          music="Primus - My Name Is Mud"
-          url="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
+          description="This Is The Worst"
+          music="Scary Sound Effect"
+          url="https://firebasestorage.googleapis.com/v0/b/video-2db61.appspot.com/o/ThisIsTheWorst.mp4?alt=media&token=4b8e6614-b454-4002-84b8-403fff8f36df"
         />
         <Video 
           likes={234}
           messages={432}
           shares={123}
           name="AkinoriKoerich"
-          description="Gato sendo filho da puta"
-          music="Primus - My Name Is Mud"
-          url="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
+          description="I'm Freeeeeee"
+          music="a"
+          url="https://firebasestorage.googleapis.com/v0/b/video-2db61.appspot.com/o/i'mFree.mp4?alt=media&token=ce9161ae-2ad4-4716-9200-47f870416484"
         />
       </div>
     </div>
--- src/pages/components/sidebar/VideoSidebar.js
+++ src/pages/components/sidebar/VideoSidebar.js
@@ -1,38 +1,50 @@
-import React from "react";
-import "./videoSidebar.css";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ChatIcon from "@mui/icons-material/Chat";
-import ShareIcon from "@mui/icons-material/Share";
-import { useState } from "react";
+import React, { useState } from 'react';
+import './videoSidebar.css';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatIcon from '@mui/icons-material/Chat';
+import ShareIcon from '@mui/icons-material/Share';
 
-function VideoSidebar({likes, messages, shares}) {
+function VideoSidebar({ likes, messages, shares }) {
   const [liked, setLiked] = useState(false);
+  const [heartAnimation, setHeartAnimation] = useState(false);
 
-  function handdleLike() {
+  const handleLike = () => {
     setLiked(!liked);
-  }
+
+    if (!liked) {
+      setHeartAnimation(true);
+
+      setTimeout(() => {
+        setHeartAnimation(false);
+      }, 1000);
+    }
+  };
 
   return (
     <div className="videoSidebar">
-      <div className="videoSideBar__option" onClick={handdleLike}>
+      <div className="videoSideBar__option" onClick={handleLike}>
         {liked ? (
-          <FavoriteIcon fontSize="large" />
+          <FavoriteIcon fontSize="medium" />
         ) : (
-          <FavoriteBorderIcon fontSize="large" />
+          <FavoriteBorderIcon fontSize="medium" />
         )}
 
         <p>{liked ? likes + 1 : likes} </p>
+
+        {heartAnimation && (
+          <FavoriteIcon className="heart-animation" fontSize="large" />
+        )}
       </div>
 
       <div className="videoSideBar__option">
-        <ChatIcon fontSize="large" />
-        <p> { messages } </p>
+        <ChatIcon fontSize="medium" />
+        <p> {messages} </p>
       </div>
 
       <div className="videoSideBar__option">
-        <ShareIcon fontSize="large" />
-        <p> { shares }</p>
+        <ShareIcon fontSize="medium" />
+        <p> {shares}</p>
       </div>
     </div>
   );
